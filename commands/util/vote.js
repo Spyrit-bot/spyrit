7d57fa3feec35317dbe4f162f78cf4d5d51ca52e
Guilherme Fonseca <souoguilhe@gmail.com>
2023-11-07 20:09:39 -0300

    Versão 1.0.7 6.1 - Melhoria do Votar

--- commands/util/vote.js
+++ commands/util/vote.js
@@ -47,20 +47,45 @@ export default {
  }
   },
   msgrun: async(bot,db,m)=>{
- let url = "https://spyrit.squareweb.app/botlist"
+    
+
+ let url = "https://bombadeagua.life/bot/1166885109471907840"
+ let userModel = db.model("user")
+ let transModel = db.model("transactions")
+ 
  let votou = await process.simo.votou(m.author.id)
+ let user = await userModel.findOneAndUpdate({ _id: m.author.id },{}, { upsert: true })
+ if(votou && !user.vote.voted){
+   user.economy.bal += 1200;
+   user.vote.voted = true
+   let trans = new transModel({
+     userid: m.author.id,
+     value: 1200,
+     type: 7,
+     timestamp: Date.now()
+   })
+   await trans.save()
+   await user.save();
+   let lc = bot.channels.cache.get(process.env.logs);if(lc){lc.send(`${m.author.globalName || m.author.username}(${m.author.tag}/${m.author.id}) ganhou ${process.formatar(1200)} por votar.`).catch(()=>{}) }
+   
+   return m.reply(`Obrigada por votar em mim! Já lhe dei ${process.formatar(1200)} em seu saldo! Lembre-se de usar </bank dep:${process.cmdid["bank"]}> para proteger seu saldo e votar em mim 2x por dia!`).catch(()=>{})
+ }
  if(votou){
    let tempo = await process.simo.tempovoto(m.author.id)
    
- return m.reply({ content: `Você já votou em mim hoje! Vote novamente <t:${Math.floor(tempo/1000)}:R>!`}).catch(()=>{})
- }
+ return m.reply({ content: `Você já votou em mim e já pegou seu prêmio hoje! Vote novamente <t:${Math.floor(tempo/1000)}:R>!`}).catch(()=>{})
+ } else {
+   user.vote.voted = true
+ 
     m.reply({
       embeds:[new EmbedBuilder()
       .setTitle(`Vote em mim`)
       .setColor("#ffffff")
-      .setDescription(`Vote em mim na [Simo's Botlist aqui](${url}) e ganhe:\n- Menos cooldown(1.5s > 1s)\n- Menos taxas no pay(5% > 3%)\n- 20% mais daily\n\nObs: caso você já tenha votado e ainda não tenha ido, espere 15 segundos que os dados antigos serão apagados.`)
+      .setDescription(`Vote em mim na [Simo's Botlist aqui](${url}) e ganhe:\n- Menos cooldown(3s > 2.1s)\n- Menos taxas no pay(5% > 3%)\n- Mais daily(1x > 1.2x)\n\nObs: caso você já tenha votado e ainda não tenha ido, espere 15 segundos que os dados antigos serão apagados.\nObs: se você votar e usar o comando novamente, você vai ganhar ${process.formatar(1200)}!`)
       ]
     }).catch(()=>{})
+    await user.save()
+ }
   },
   
 }
