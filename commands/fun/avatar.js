a71cf10eb47f2e46f1c8b055b1752b6dc394f1b1
Guilherme Fonseca <souoguilh@gmail.com>
2023-11-11 13:27:32 -0300

    Comando de Avatar para Prefixo

--- commands/fun/avatar.js
+++ commands/fun/avatar.js
@@ -33,5 +33,31 @@ export default {
             )
           ]
     }).catch(()=>{})
-  }
+  },
+  msgrun: async (bot, db, m, args)=>{
+    let usr = m.author;
+ if(args[0]) usr = m.mentions.users.first() || bot.users.cache.get(args[0]) || bot.users.cache.find(b=>b?.globalName?.toLowerCase()?.includes(args.join(" ")?.toLowerCase()) || b?.tag?.toLowerCase()?.includes(args.join(" ")?.toLowerCase())) || m.author
+    let ius = usr.id === m.author.id;
+    m.reply({
+      embeds: [
+        new EmbedBuilder()
+        .setTitle(ius ? `Seu avatar` : `Avatar de ${usr.tag}`)
+        .setImage(usr.avatarURL({ size: 4096, format: `PNG`}))
+        .setFooter({
+          text: ius ? `Ainda é seu avatar` : `Ainda é o avatar de ${usr.tag}`
+        })
+        .setColor("#ffffff")
+        ],
+        components:[
+          new ActionRowBuilder()
+          .addComponents(
+            new ButtonBuilder()
+            .setStyle(ButtonStyle.Link)
+            .setLabel(`Abrir no navegador`)
+            .setURL(usr.avatarURL({ size: 4096, format: `PNG`}))
+            )
+          ]
+    }).catch(()=>{})
+  },
+  
 }
